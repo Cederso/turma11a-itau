73b0bc3b93a777ef2c9a9a87bf240dfd9022e29a
Cederso <74359200+Cederso@users.noreply.github.com>
2020-11-19 14:19:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
 
-<h5>Mudando agora com o pessoal da turma</h5>
-<h4>
+<h5>Mudando agora com o pessoal da turma 1000</h5>
+<h4> 
   <? 
   $i = 6;
   $i = $i * 350;
